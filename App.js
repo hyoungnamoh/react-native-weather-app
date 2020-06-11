8f2226e848754b49cdfe92992e2bdb7c5c7a4ed0
hyoungnamoh <zzzsh789@naver.com>
2020-06-11 23:43:31 +0900

    Add:axios, 날씨  API 호출

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState, Fragment } from 'react';
 import { StyleSheet, Text, View, Alert } from 'react-native';
 import Loading from './Loading';
 import * as Location from 'expo-location';
+import axios from 'axios';
+
+const API_KEY = '9bd6a913e664c38cf72305fa0d228e90';
 
 const App = () => {
   const [isLoading, setIseLoading] = useState(true);
@@ -10,12 +13,16 @@ const App = () => {
     getLocation();
   }, [])
 
+  const getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=-${longitude}&appid=${API_KEY}`);
+    console.log(data);
+  }
   const getLocation = async () => {
     try {
       await Location.requestPermissionsAsync();
       const {coords} = await Location.getCurrentPositionAsync();
-      setIseLoading(false);
-      console.log(coords.latitude, coords.longitude);
+      // setIseLoading(false);
+      getWeather(coords.latitude, coords.longitude);
     } catch (error) {
       Alert.alert('사용자 권한 에러', error);
     }
